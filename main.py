bb06b00143d45e0af99aa329a2adad00398f6ce1
ImNotThatPro <duonglonglangson@gmail.com>
2025-11-14 22:18:19 +0700

    Adding green boxes to motion that are detected

--- main.py
+++ main.py
@@ -17,9 +17,16 @@ while True:
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
     frame_diff = cv2.absdiff(prev_gray, gray)
-
     _, thresh = cv2.threshold(frame_diff, 25, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    for contour in contours:
+        if cv2.contourArea(contour) < 500:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x,y), (x + w, y + h), (0, 255, 0), 2)
+
+    
     combined = cv2.hconcat([frame, cv2.cvtColor(thresh, cv2.COLOR_BAYER_BG2BGR)])
     cv2.imshow('Motion Detection', combined)
 
