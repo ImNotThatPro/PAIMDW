e8942ae44308f06b5c74990866c941eeb68a5a05
ImNotThatPro <duonglonglangson@gmail.com>
2025-11-14 22:12:56 +0700

    Adding a raw motion detection

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+ret, prev_frame =cap.read()
+prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+prev_gray = cv2.GaussianBlur(prev_gray, (21, 21), 0)
 
 while True:
     ret, frame = cap.read()
@@ -10,7 +13,17 @@ while True:
         print('Failed to grab frame')
         break
 
-    cv2.imshow('Webcam Feed', frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+    frame_diff = cv2.absdiff(prev_gray, gray)
+
+    _, thresh = cv2.threshold(frame_diff, 25, 255, cv2.THRESH_BINARY)
+
+    combined = cv2.hconcat([frame, cv2.cvtColor(thresh, cv2.COLOR_BAYER_BG2BGR)])
+    cv2.imshow('Motion Detection', combined)
+
+    prev_gray = gray.copy()
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
